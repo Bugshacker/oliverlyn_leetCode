b8b1580d5a704d8f43827499b913404d16d22a2d
Oliverlyn <warriorlyn@gmail.com>
2016-02-24 09:13:50 +0800

    Update powerOfThree.cpp

--- powerOfThree.cpp
+++ powerOfThree.cpp
@@ -9,3 +9,16 @@ public:
     }
    
 };
+/*************
+*power of two*
+**************
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        else if(n >= 1) {
+            return pow(2, round(log(n)/log(2))) == n;
+        }
+    }
+};*/
